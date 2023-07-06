252af748384d467e8c4756a53aae1d020e7b6133
Kenny Martinez <kenny@Kennys-MacBook-Pro.local>
2023-07-06 11:32:28 -0500

    Initial commit

---
+++ GloboTicket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,7 @@
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Persistence;
+
+public interface IOrderRepository : IAsyncRepository<Order>
+{
+}
---
+++ GloboTicket.TicketManagement.Application/Features/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+
+namespace GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCategory;
+
+public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+{
+    public CreateCategoryCommandValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .NotNull()
+            .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+    }
+}
